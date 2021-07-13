467b965934fd6ce9f278a865e46d17d4b1271001
Promise <promiseprince01@gmail.com>
2021-07-13 17:07:59 +0100

    added new features

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class  Counter extends Component {
   state = {
-    counter: this.props.value,
+    counter: this.props.counter.value,
     tags: ['shoe1', 'shoe2', 'shoe3']
   };
 
@@ -12,7 +12,7 @@ class  Counter extends Component {
   // }
 
   handleIncrement = (product) => {
-    console.log(product);
+    this.props.counter.value = 0;
     this.setState( { counter: this.state.counter + 1});
   }
 
@@ -21,13 +21,11 @@ class  Counter extends Component {
   }
 
   render() {
-
-    console.log("props", this.props);
-
     return (
     <React.Fragment>
       <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
       <button onClick={this.doHandleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+      <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
     </React.Fragment>
       );
   }
